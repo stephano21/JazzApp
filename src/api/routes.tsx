8da7e5997a03143a3859d8e9208b44c796c99439
Stephano <stephanochang21@gmail.com>
2024-03-19 09:23:27 -0500

    Update routes.tsx

--- src/api/routes.tsx
+++ src/api/routes.tsx
@@ -1,5 +1,5 @@
 export const Endpoints = {
-  BaseURL: 'https://dddc-157-100-158-182.ngrok-free.app',
+  BaseURL: 'https://e08e-157-100-158-182.ngrok-free.app',
   BaseApi: '/api',
   login: '/auth/login/',
   register: '/auth/register/',
